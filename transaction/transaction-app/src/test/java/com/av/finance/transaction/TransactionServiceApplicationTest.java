08503351bbb1659edc1212375a6a63aa6c906855
Aleksey Vtyurin <About269!@>
2022-05-22 15:11:10 +0300

    Add web, app modules, extend modules pom.xml

--- transaction/transaction-app/src/test/java/com/av/finance/transaction/TransactionServiceApplicationTest.java
+++ transaction/transaction-app/src/test/java/com/av/finance/transaction/TransactionServiceApplicationTest.java
@@ -1,4 +1,4 @@
-package com.av.finance.transaction.infrastructure;
+package com.av.finance.transaction;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
