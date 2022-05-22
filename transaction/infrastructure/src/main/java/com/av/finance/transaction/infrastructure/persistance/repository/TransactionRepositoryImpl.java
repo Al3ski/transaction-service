996854508356017937f9fb586fb5a892f9a61e98
Aleksey Vtyurin <About269!@>
2022-05-22 18:39:55 +0300

    Add transaction repository implementation, dao, mapper, entity

--- transaction/infrastructure/src/main/java/com/av/finance/transaction/infrastructure/persistance/repository/TransactionRepositoryImpl.java
+++ transaction/infrastructure/src/main/java/com/av/finance/transaction/infrastructure/persistance/repository/TransactionRepositoryImpl.java
@@ -2,26 +2,43 @@ package com.av.finance.transaction.infrastructure.persistance.repository;
 
 import com.av.finance.transaction.domain.transaction.Transaction;
 import com.av.finance.transaction.domain.transaction.repository.TransactionRepository;
+import com.av.finance.transaction.infrastructure.persistance.dao.TransactionDao;
+import com.av.finance.transaction.infrastructure.persistance.entity.TransactionEntity;
+import com.av.finance.transaction.infrastructure.persistance.mapper.TransactionMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
+@RequiredArgsConstructor
 @Component
 public class TransactionRepositoryImpl implements TransactionRepository {
 
+    private final TransactionDao transactionDao;
+    private final TransactionMapper mapper;
+
     @Override
     public Transaction retrieve(UUID txId) {
-        return null;
+        final TransactionEntity entity = transactionDao.findById(txId).orElse(null);
+        final Transaction transaction = mapper.toTransaction(entity);
+        log.debug("Found transaction: {} for tx: {}", transaction, txId);
+        return transaction;
     }
 
     @Override
     public List<Transaction> retrieveByAccount(UUID accountId) {
-        return null;
+        final List<TransactionEntity> entities = transactionDao.findAllByAccountId(accountId);
+        final List<Transaction> transactions = mapper.toTransactions(entities);
+        log.debug("Found transactions: {} for account: {}", transactions, accountId);
+        return transactions;
     }
 
     @Override
     public void save(Transaction transaction) {
-
+        transactionDao.save(mapper.toEntity(transaction));
+        log.debug("Transaction: {} successfully created", transaction);
     }
 }
